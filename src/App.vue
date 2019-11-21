00eebde367d353d10006c420acb454753a60b30d
sumiao <15976593304@163.com>
2019-11-21 15:40:23 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,21 @@
   <div id="app">
     121212
     <el-button icon="el-icon-search" circle></el-button>
-     <el-button type="primary" icon="el-icon-edit" circle></el-button>
-  <el-button type="success" icon="el-icon-check" circle></el-button>
+    <el-button type="primary" icon="el-icon-edit" circle></el-button>
+    <el-button type="success" icon="el-icon-check" circle></el-button>
+    121212
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
